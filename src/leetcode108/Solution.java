4ad6f39c6978cd1557ee885c188c9c074fbff8c9
Eivense <hexplosives96@gmail.com>
2019-09-04 00:12:46 +0800

    完成 原理还得再看看

--- src/leetcode108/Solution.java
+++ src/leetcode108/Solution.java
@@ -29,6 +29,19 @@ package leetcode108;
 
 public class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length);
+    }
 
+    private TreeNode sortedArrayToBST(int[] nums,int start,int end){
+        if(start==end)
+            return null;
+        // 求中点
+        int mid = (start + end)>>>1;
+        // 中点作为根
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, start, mid );
+        root.right = sortedArrayToBST(nums, mid + 1, end);
+
+        return root;
     }
 }
