d938d5553da5c5779744fa68a9cdcbc4992faffe
Eivense <hexplosives96@gmail.com>
2020-02-25 00:11:05 +0800

    继续刷题

---
+++ src/leetcode169/Solution.java
@@ -0,0 +1,52 @@
+package leetcode169;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * 169.多数元素
+ *
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ *
+ *
+ */
+public class Solution {
+
+    public int majorityElement(int[] nums){
+        int n = nums.length;
+
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
+            }else{
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n / 2) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int nums[] = new int[]{2, 2, 1, 1, 1, 2, 2};
+        System.out.println(solution.majorityElement(nums));
+    }
+}
